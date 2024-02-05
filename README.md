5b7ae0f074845d8ee6dc52a1b171f6aaf1957ce3
AsaadAlmughrabi <assadalmograbe8@gmail.com>
2024-02-05 11:12:59 +0000

    added some routes to the website and connect to the third Api

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 **Author Name**: Assad Almughrabi
 
 ## WRRC
-![WRRC](./WRRC.png)
+![WRRC](./images/WRRC.png)
 
 ## Overview
-<p>It's a movies website that help you to watch what you need free withou Ads</p>
+<p>It's a movies website that help you to watch what you need free without Ads</p>
 
 ## Getting Started
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
@@ -15,11 +15,16 @@
 * create index.js file , name is optional
 * npm install express to install express package to use it
 * run the server using node index.js
-*  require express framework 
+* require express framework 
 * invoke express
 * specify port number
 * run server make it liseting
+* install dot env
+* intall cors
+* add routes 
+* send req from server to api useing axios
 
 
 ## Project Features
 <!-- What are the features included in you app -->
+<p>the website will have many features like searech for a certain movies and see the top rated and upcoming movies</p>
--- server.js
+++ server.js
@@ -1,30 +1,151 @@
+"use strict";
 const express = require("express");
 const app = express();
-const port = 3001;
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const port = process.env.PORT;
+const apiKey = process.env.API_KEY;
 let movieData = require("./Movie-Data/data.json");
 
+//routes
+app.get("/", homePageHandler);
+app.get("/favorite", favoriteHandler);
+app.get("/trending", trendingHandler);
+app.get("/search", searchMovies);
+app.get("/topRated", topRatedHandler);
+app.get("/upcoming", upcomingHandler);
+
+//functions
+
+//http://localhost:8080/
+function homePageHandler(req, res) {
+  let movie = new Movies(
+    movieData.title,
+    movieData.poster_path,
+    movieData.overview
+  );
+  res.json(movie);
+}
+
+//http://localhost:8080/favorit
+function favoriteHandler(req, res) {
+  res.send("Welcome to Favorite Page");
+}
+
+//http://localhost:8080/trending
+function trendingHandler(req, res) {
+  let url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
+  //axsios
+  axios
+    .get(url)
+    .then((result) => {
+      let trendingMOvies = result.data.results.map((movie) => {
+        return new Trending(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+      });
+      res.json(trendingMOvies);
+    })
+    .catch(error=>{
+      console.log(error)
+      res.status(500).send('Internal Server Error');
+  })
+}
+
+//http://localhost:8080/search?name=put the name here
+function searchMovies(req, res) {
+  let movieName = req.query.name;
+  let url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${apiKey}`;
+  axios
+    .get(url)
+    .then((movie) => {
+      let resultMovie = movie.data.results.map((element) => {
+        return new Trending(
+          element.id,
+          element.title,
+          element.release_date,
+          element.poster_path,
+          element.overview
+        );
+      });
+      res.json(resultMovie);
+    })
+    .catch(error=>{
+      console.log(error)
+      res.status(500).send('Internal Server Error');
+  })
+}
+
+//http://localhost:8080/upcoming?page=put the number of page
+function upcomingHandler(req, res) {
+  let numOfPage = req.query.page;
+  let url = `https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${numOfPage}&api_key=${apiKey}`;
+  axios
+    .get(url)
+    .then((upcoming) => {
+      let upcomingMovie = upcoming.data.results.map((movie) => {
+        return new Trending(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+      });
+      res.json(upcomingMovie);
+    })
+    .catch(error=>{
+      console.log(error)
+      res.status(500).send('Internal Server Error');
+  })
+}
+
+//http://localhost:8080/topRated?page=put any number of page
+function topRatedHandler(req, res) {
+  let numOfPage = req.query.page;
+  let url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${numOfPage}&api_key=${apiKey}`;
+  axios
+    .get(url)
+    .then((topRated) => {
+      let tops = topRated.data.results.map((result) => {
+        return new Trending(
+          result.id,
+          result.title,
+          result.release_date,
+          result.poster_path,
+          result.overview
+        );
+      });
+      res.json(tops);
+    })
+
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Internal Server Error');
+    });
+}
+
+//constructers
 function Movies(title, poster_path, overview) {
   this.title = title;
   this.poster_path = poster_path;
   this.overview = overview;
 }
 
-app.get("/", homePageHandler);
-
-function homePageHandler(req, res) {
-  let movie=new Movies(movieData.title,movieData.poster_path,movieData.overview);
-  res.json(movie);
+function Trending(id, title, release_date, poster_path, overview) {
+  (this.id = id),
+    (this.title = title),
+    (this.release_date = release_date),
+    (this.poster_path = poster_path),
+    (this.overview = overview);
 }
 
-app.get("/favorite", favoriteHandler);
-function favoriteHandler(req, res) {
-  res.send("Welcome to Favorite Page");
-}
-
-app.listen(port, () => {
-  console.log(`server is running and  listening on port ${port}`);
-});
-
+// 500 error handler
 function handleServerError(err, req, res, next) {
   res.status(500);
   res.json({
@@ -33,6 +154,7 @@ function handleServerError(err, req, res, next) {
   });
 }
 
+//404 error handler
 function handlerError(err, req, res, next) {
   res.status(404);
   res.json({
@@ -40,5 +162,10 @@ function handlerError(err, req, res, next) {
     responseText: "Sorry, something went wrong",
   });
 }
-app.use(handlerError);
+
+ app.use(handlerError);
 app.use(handleServerError);
+
+app.listen(port, () => {
+  console.log(`server is running and  listening on port ${port}`);
+});
